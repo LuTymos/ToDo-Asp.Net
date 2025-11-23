2a06343984b47f94d18bc04b18c6409e013704b1
LucasTymos <lucastymos@gmail.com>
2025-11-23 01:22:30 -0300

    feat: initial project setup with docker orchestration and database context

---
+++ Data/Contexts/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lucas.Api.ToDo.Data.Contexts
+{
+    public class DatabaseContext : DbContext
+    {
+
+
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected DatabaseContext()
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Lucas.Api.ToDo.Data.Contexts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+#region INICIALIZANDO O BANCO DE DADOS
+var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection");
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+#endregion
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
